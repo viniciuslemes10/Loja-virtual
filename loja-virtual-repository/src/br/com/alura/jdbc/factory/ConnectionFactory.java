ce4ed00661d7e754ca1fdd520170a35ba5a219f4
Vinicius <115731879+viniciuslemes10@users.noreply.github.com>
2022-12-20 12:38:38 -0300

    Add files via upload

---
+++ loja-virtual-repository/src/br/com/alura/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,35 @@
+package br.com.alura.jdbc.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("Vl102030@");
+
+		comboPooledDataSource.setMaxConnectionAge(15);
+
+		this.dataSource = comboPooledDataSource;
+
+	}
+
+	public Connection recuperaConexao() {
+		try {
+			return this.dataSource.getConnection();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+
+	}
+}
